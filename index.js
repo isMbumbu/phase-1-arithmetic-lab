53172dd4eccccd0836b65a02d67a0f3083c4c12b
isMbumbu <andrewambuka9@gmail.com>
2024-09-26 15:06:48 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 
 // Write your code here
+/*Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+Create a variable called random that will generate a random integer greater than 0.
+Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+Create a variable called max that finds the highest number in a set; the value returned should be 20.*/
+
+// function multiply(num1, num2) {
+//     return num1* num2;
+// }
+// function random() {
+//     return Math.floor(Math.random() * 100) + 1; // Example range
+// }
+
+// function mod(num1, num2) {
+//     return num1 % num2;
+// }
+
+// function max(arr) {
+//     return Math.max(...arr);
+// }
+let num1=31;
+let num2=2;
+let multiply=num1 *num2;
+let random=Math.floor(Math.random() * 100) + 1;
+let num3=9
+let num4=5
+let mod=num3%num4;
+let max=Math.max(9,4,5,20);
+
